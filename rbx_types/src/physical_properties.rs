f042cdb5d42eacba92626ad4915c50ce18382e00
John Hui <11800204+j-hui@users.noreply.github.com>
2023-08-01 10:42:21 -0700

    Implement physical properties...

--- rbx_types/src/physical_properties.rs
+++ rbx_types/src/physical_properties.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
+#[cfg(feature = "mlua")]
+use mlua::prelude::*;
+
 /// Represents the physical properties that parts can have.
 ///
 /// Equivalent to Roblox's [`PhysicalProperties`][PhysicalProperties] type, with
@@ -35,6 +38,49 @@ pub struct CustomPhysicalProperties {
     pub elasticity: f32,
     pub friction_weight: f32,
     pub elasticity_weight: f32,
+}
+
+impl<'lua> FromLua<'lua> for PhysicalProperties {
+    fn from_lua(value: LuaValue<'lua>, _lua: &'lua Lua) -> LuaResult<Self> {
+        let LuaValue::UserData(value) = value else {
+            return Err(LuaError::UserDataTypeMismatch);
+        };
+        if !value.is::<Self>() {
+            return Err(LuaError::UserDataTypeMismatch);
+        }
+
+        // Note: this does not reflect a PhysicalProperties::Default back into itself.
+        // Instead, it will create a PhysicalProperties::Custom with Default values for all fields.
+        Ok(Self::Custom(CustomPhysicalProperties {
+            density: value.get("Density")?,
+            friction: value.get("Friction")?,
+            elasticity: value.get("Elasticity")?,
+            friction_weight: value.get("FrictionWeight")?,
+            elasticity_weight: value.get("ElasticityWeight")?,
+        }))
+    }
+}
+
+impl LuaUserData for PhysicalProperties {
+    fn add_fields<'lua, F: LuaUserDataFields<'lua, Self>>(fields: &mut F) {
+        // Note: at least in Roblox studio, Default materials seem to return 0.0 for every field.
+        macro_rules! custom_field {
+            ($lua_name:literal, $rust_name:ident) => {
+                fields.add_field_method_get($lua_name, |_lua, this| {
+                    Ok(match this {
+                        PhysicalProperties::Default => 0.0,
+                        PhysicalProperties::Custom(this) => this.$rust_name,
+                    })
+                });
+            };
+        }
+
+        custom_field!("Density", density);
+        custom_field!("Friction", friction);
+        custom_field!("Elasticity", elasticity);
+        custom_field!("FrictionWeight", friction_weight);
+        custom_field!("ElasticityWeight", elasticity_weight);
+    }
 }
 
 #[cfg(feature = "serde")]
